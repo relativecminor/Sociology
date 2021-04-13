cce96b37cc086e108154e46944e1bb72abdf224b
Cameron Minor <minorcj@hendrix.edu>
2021-04-12 20:28:26 -0500

    Added money variable and money changing method

--- SociologyProject/Assets/Scripts/GameManager.cs
+++ SociologyProject/Assets/Scripts/GameManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
+    public GameObject moneyTextBox;
+
+    public int money;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,4 +21,12 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void ChangeMoney(int amount)
+    {
+        money = money + amount;
+        if (money < 0) { money = 0; }
+        moneyTextBox.GetComponent<TextMeshProUGUI>().text = "Money: " + money.ToString();
+    }
+
 }
