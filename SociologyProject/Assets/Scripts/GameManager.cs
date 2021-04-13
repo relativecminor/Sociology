c81affcba9e6d64603f460e271956e01459661a1
Cameron Minor <minorcj@hendrix.edu>
2021-04-12 20:34:18 -0500

    Added "morality" variable and restructured some money stuff

--- SociologyProject/Assets/Scripts/GameManager.cs
+++ SociologyProject/Assets/Scripts/GameManager.cs
@@ -9,11 +9,12 @@ public class GameManager : MonoBehaviour
     public GameObject moneyTextBox;
 
     public int money;
+    private int morality;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        morality = 0;
     }
 
     // Update is called once per frame
@@ -25,8 +26,13 @@ public class GameManager : MonoBehaviour
     public void ChangeMoney(int amount)
     {
         money = money + amount;
-        if (money < 0) { money = 0; }
+        if (money < 0 || money > 9999999) { money = 0; }
         moneyTextBox.GetComponent<TextMeshProUGUI>().text = "Money: " + money.ToString();
+    }
+
+    public void ChangeMOrality(int amount)
+    {
+        morality = morality + amount;
     }
 
 }
