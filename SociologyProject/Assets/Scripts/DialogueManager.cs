52d3ad80544018c6bc228ddc11f94aab2adb3d35
olivia <larsonog@hendrix.edu>
2021-04-15 23:42:27 -0500

    Started to add a tutorial

--- SociologyProject/Assets/Scripts/DialogueManager.cs
+++ SociologyProject/Assets/Scripts/DialogueManager.cs
@@ -15,14 +15,17 @@ public class DialogueManager : MonoBehaviour
     public GameObject dialogueBox;
     public GameObject textBox;
 
-    private string[] chapter1Dialogue = new string[2];
+    private string[] chapter1Dialogue = new string[5];
     private int currentTextTracker = 0;
 
     void Start()
     {
         policyCanvas.enabled = false;
-        chapter1Dialogue[0] = "As the school year draws to a close and the weather is getting warmer, it's time for School High to select the teacher training programs for the summer.";
-        chapter1Dialogue[1] = "As the education coordinator for your district the selection falls on your hands, and there are a couple factors to consider";
+        chapter1Dialogue[0] = "Hello and Welcome to GAME NAME. You will be playing as the government and as the superintendent of a school. You will be makigng decisions that have a lot of impact.";
+        chapter1Dialogue[1] = "Use the policy menu to discover policies that you can pass as the government. Be careful, you must have enough money to buy and inact the policy";
+        chapter1Dialogue[2] = "Also be weary of its effect on students in the school system. Some policies can have devestating effects on the student population";
+        chapter1Dialogue[3] = "As the school year draws to a close and the weather is getting warmer, it's time for School High to select the teacher training programs for the summer.";
+        chapter1Dialogue[4] = "As the education coordinator for your district the selection falls on your hands, and there are a couple factors to consider";
     }
 
     public void OpenBox()
