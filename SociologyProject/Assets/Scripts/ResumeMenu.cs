eb4f794d77eaec68c8e3940f8748558657543af7
Dmitry Vyakhirev <vyakhirevdd@hendrix.edu>
2021-04-25 23:10:17 -0500

    Resume menu triggered on Escape

--- SociologyProject/Assets/Scripts/ResumeMenu.cs
+++ SociologyProject/Assets/Scripts/ResumeMenu.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ResumeMenu : MonoBehaviour
 {
+    Canvas canvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas = gameObject.GetComponent<Canvas>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!canvas.enabled)
+            {
+                canvas.enabled = true;
+            }
+            else
+            {
+                canvas.enabled = false;
+            }
+        }
+
         
     }
 
@@ -20,4 +34,5 @@ public class ResumeMenu : MonoBehaviour
     {
         StartCoroutine(GameManager.Instance.LoadYourAsyncScene("MainMenu"));
     }
+
 }
